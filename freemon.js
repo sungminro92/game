41c84e26f020ef9d815c9388a9fa3a0dbb788d87
tjdals504 <sungminro92@gmail.com>
2017-03-08 22:30:27 -0500

    Update javascript structure to support the game

--- freemon.js
+++ freemon.js
@@ -1,6 +1,59 @@
-// Array of words in the game
+// Array of word possibilities in the game
 const easyWords = ['PICKACHU','IVY','CHARMANDER','BULBASAUR','SQUIRTLE','BUTTERFREE','PIDGEY','PSYDUCK','CLEFAIRY','DIGLETT'];
 const hardWords = ['CLOYSTER','ALAKAZAM','KANGASKHAN','RHYDON','GYRADOS','TYPHLOSION','WEEZING','EXEGGUTOR','WIGGLYTUFF','MACHAMP'];
+
+
+// Data & Core Game Logic//
+const GameStatusData = {
+    user: [], // list of users
+    isPlaying: false, // the status of the game
+    playerTurn: 0, // indicates 'who is playing' supports multiplayers
+    difficultyLevel: null, // refers to 'difficulty level'
+
+    setDifficultyLevel(level) {
+      this.difficultyLevel = level;
+    },
+
+    addUser(name) {
+      this.user.push({
+        userName: name,
+        score: 0,
+      });
+    },
+    startGame(){
+      if (this.isPlaying == false) {
+      this.isPlaying = true;
+      }
+
+    },
+
+    endGame(){
+      if (this.isPlaying !== false) {
+        this.isPlaying = false;
+      }
+    },
+
+    resetGame() {
+      this.user = [];
+      this.isPlaying = false;
+      this.playerTurn: 0;
+      difficultyLevel: null;
+    }
+};
+
+// User Interface //     
+const ViewEngine = {
+
+};
+
+// Top Level Application Code //
+const GameController = {
+  handleGameStart() {
+    
+  }
+}
+
+
 
 // This function will pick word.
 function generateEmptyWordBox() {
